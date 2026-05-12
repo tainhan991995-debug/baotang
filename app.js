c3b01b6934ba4e7b7b682695a37b71976e64093f
tainhan991995-debug <tainhan991995@gmail.com>
2026-05-12 15:49:24 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,123 @@
+const API_TOKEN =
+  "https://cqs.hue.gov.vn/taikhoan/getToken";
+
+const API_DATA =
+  "https://cqs.hue.gov.vn/data/dulieuso";
+
+let token = "";
+
+// ================= TOKEN =================
+
+async function getToken(){
+
+  const res = await fetch(API_TOKEN, {
+
+    method:"POST",
+
+    headers:{
+      "Content-Type":"application/json"
+    },
+
+    body:JSON.stringify({
+
+      username:"Baotang1",
+
+      password:"Baotang123"
+
+    })
+  });
+
+  const data = await res.json();
+
+  token = data.accessToken;
+
+  console.log("TOKEN:", token);
+}
+
+// ================= PLACES =================
+
+async function getPlaces(){
+
+  if(!token){
+    await getToken();
+  }
+
+  const res = await fetch(API_DATA, {
+
+    method:"POST",
+
+    headers:{
+      "Content-Type":"application/json",
+      token:token
+    },
+
+    body:JSON.stringify({
+
+      serviceid:"kNLicdSB8f5wyIpDGXIHwg==",
+
+      thamso:{},
+
+      page:1,
+
+      perpage:50
+    })
+  });
+
+  const result = await res.json();
+
+  console.log("PLACES:", result);
+
+  return result.data || [];
+}
+
+// ================= IMAGES =================
+
+async function getImages(placeId){
+
+  if(!token){
+    await getToken();
+  }
+
+  const res = await fetch(
+    "https://cqs.hue.gov.vn/data/danhsach",
+    {
+
+      method:"POST",
+
+      headers:{
+        "Content-Type":"application/json",
+        token:token
+      },
+
+      body:JSON.stringify({
+
+        serviceid:
+          "Eurka/RmTXuwLAMXDe5fGA==",
+
+        thamso:{
+          tukhoa:"",
+          eformid:"0"
+        },
+
+        page:"1",
+
+        perpage:"50"
+      })
+    }
+  );
+
+  const result = await res.json();
+
+  console.log("API ẢNH:", result);
+
+  const images =
+    (result.data || []).filter(item => {
+
+      return String(item.teneformid) ===
+             String(placeId);
+    });
+
+  console.log("ẢNH:", images);
+
+  return images;
+}
